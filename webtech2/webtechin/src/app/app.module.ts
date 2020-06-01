21228d4af9064a4fa26f33204c5b498131002aa0
Balx0 <balx99@gmail.com>
2020-06-01 20:21:37 +0200

    angular update

--- webtech2/webtechin/src/app/app.module.ts
+++ webtech2/webtechin/src/app/app.module.ts
@@ -8,18 +8,25 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ListItemService } from './list-items.service';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ListItemsComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatInputModule,
   ],
   providers: [
     ListItemService
